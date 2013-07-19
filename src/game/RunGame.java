d05328b764b6bef5c09ec4fa11e6f85557218194
tim <tjhickey@brandeis.edu>
2013-07-19 14:35:54 -0400

    updated version number to 1.0.1

--- src/game/RunGame.java
+++ src/game/RunGame.java
@@ -17,7 +17,7 @@ import javax.swing.JSlider;
  *
  */
 public class RunGame {
-	public static String versionNum = "1.0.0";
+	public static String versionNum = "1.0.1 (7/19/2013)";
 	
 	public static void main(String[] args) { 
 		GameSpec gs = new GameSpec();
