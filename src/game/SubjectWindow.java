d3b38326a02141ea3a47ca4095ad59402ab6f0cd
tim <tjhickey@brandeis.edu>
2013-07-21 09:08:02 -0400

    refactored SubjectWindow to remove unused buttons and panels

--- src/game/SubjectWindow.java
+++ src/game/SubjectWindow.java
@@ -16,31 +16,14 @@ import javax.swing.event.*;
  *
  */
 public class SubjectWindow extends JFrame {
-	private GameModel gm;
 
-	public JFrame frame;
-	
+	public JFrame frame;	
 	public GameView gameboard;
 	private JLabel status;
-	private JSlider speedSlider;
-
-	// this will listen to timer events
-	// and update the game and view every timestep
-	private ActionListener stepButtonListener;
 	
 
 	public SubjectWindow(GameModel gamemodel) { // throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-
-
-		JPanel buttonPanel;
-		JButton stepButton;
-		JButton runButton;
-		JButton stopButton;
-		JButton resetButton;
 		
-		this.gm=gamemodel;
-		//this window will have the settings we can adjust
-
 		// first we create the Frame with a border layout
 		frame = this;
 		this.setTitle("Subject Window");
@@ -49,59 +32,9 @@ public class SubjectWindow extends JFrame {
 		frame.setLayout(new BorderLayout());
 		
 		// next we create the gameview
-		gameboard = new GameView(gm);
+		gameboard = new GameView(gamemodel);
 		
-		
-		// here is the title of the game and the status bar
-		status = new JLabel("Try to collect all the fish!");
-		status.setForeground (Color.red);
-		
-		// Create the buttons and add actions
-		buttonPanel = new JPanel();
-		buttonPanel.setLayout(new GridLayout(2,1));
-		stepButton = new JButton();
-		stepButton.setText("step");
-
-		stepButton.addActionListener(stepButtonListener);
-
-		runButton = new JButton();
-		runButton.setText("run");
-		runButton.addActionListener(
-				new ActionListener(){
-					public void actionPerformed(ActionEvent e){
-						gm.start();
-						gm.gameOver=false;
-						gm.writeToLog(new GameEvent("startgame"));
-						status.setText("game in play");
-					}
-				});
-		stopButton = new JButton("stop");
-		stopButton.addActionListener(
-			new ActionListener(){
-				public void actionPerformed(ActionEvent e){
-					status.setText("game over");
-					//gm.writeToLog(new GameEvent("stopgame"));
-					//gm.stop();
-				}
-			});
-		
-		resetButton = new JButton("reset");
-		resetButton.addActionListener(
-				new ActionListener(){
-					public void actionPerformed(ActionEvent e){
-	//					gm.initActors();
-						status.setText("game reset!");
-						gm.gameOver=false;
-					}
-				});
 		status = new JLabel("");
-		
-		buttonPanel.add(runButton);
-		buttonPanel.add(stopButton);
-
-		
-
-	
 		
 		// put the frame components together with a border layout
 		frame.add(gameboard.header,BorderLayout.NORTH);
