6330b9be0365521b713cc4850b6719f927f6f3be
Yile <ylesun@brandeis.edu>
2013-07-26 10:05:15 -0400

    new set of fish sound with brown noise at intensity of 0.2 fish speed is 40 now, 4 times as fast as the original

--- src/game/GameActor.java
+++ src/game/GameActor.java
@@ -27,7 +27,7 @@ public class GameActor {
 		// still on board?
 		boolean active;
 		// speed
-		double speed=10;
+		double speed=40;
 		// species
 		boolean fromLeft; // true if fish comes from left
 		long birthTime;
