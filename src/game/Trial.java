84f5691f502d153fab79d8670870b74f01b76e9a
tim <tjhickey@brandeis.edu>
2013-09-06 12:23:28 -0400

    added some documentation

--- src/game/Trial.java
+++ src/game/Trial.java
@@ -1,10 +1,33 @@
 package game;
 
 import java.util.ArrayList;
-
+/**
+ * A Trial is a Fish Event, e.g. 
+ *   Good Fish from Right
+ *   Bad Fish from Left
+ * and it has a fixed amount of time. 
+ * In fMRI mode, the previous trial ends
+ * with the fish disappearing from the screen
+ * The new trial begins with no fish,
+ * and the after an inter-fish interval the fish appears
+ * @author tim
+ *
+ */
 public class Trial {
+	/**
+	 * an ArrayList storing the properties of the trial
+	 */
 	public ArrayList<Object> specs = new ArrayList<Object>();
 
+	/**
+	 * 
+	 * @param interval time in nanoseconds from beginning of the trial
+	 * @param soundFile  name of the sound file in the sounds/ folder
+	 * @param visualHz the throb rate
+	 * @param congruent true if the sound hertz matches the visual hertz
+	 * @param fromLeft  true if fish comes from the left
+	 * @param spec the Species of fish (i.e. good or bad) correlated with the visualHz field sort of ...
+	 */
 	public Trial(Long interval, String soundFile, int visualHz,
 			boolean congruent, Boolean fromLeft, Species spec) {
 		specs.add(0, interval);
