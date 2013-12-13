4c0ccfb45b19f14f1105bc3c40964d403792afb1
fatima <fatima@129.64.235.129>
2013-12-13 02:08:06 -0500

    make the splash screen not flashing using card layout

--- src/game/RunGame.java
+++ src/game/RunGame.java
@@ -1,5 +1,6 @@
 package game;
 
+import java.awt.CardLayout;
 import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Image;
@@ -11,11 +12,13 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 
 /**
- * This creates the experimenter's window, which offers the user a choice to run
+ * this create the splash screen for the game in addition 
+ * it creates the experimenter's window, which offers the user a choice to run
  * from a script or generate a script. It also stores the versionNum
  * 
  * @author tim
@@ -29,7 +32,7 @@ public class RunGame {
 	 * the Major/Minor/Bugfix versions of the code.
 	 */
 	public static String versionNum = "3";
-
+    public static final String filename="images/fish/smallfish.png";
 	public static void main(String[] args) throws InterruptedException {
 		 JPanel panel = new JPanel(null)
 	        {
@@ -42,27 +45,50 @@ public class RunGame {
 	                super.paintComponent(g);
 	            }
 	        };
+	        
+//	      panel.add( new AnimationBackground(10, 10, 2, 3, 1, 1, 10) );
+	        
+	        JPanel panel2 = new JPanel(null)
+	        {
+	            Image image = new ImageIcon("images/menueback2.png").getImage();
+	            Image image2=new ImageIcon("Images/stream.jpg").getImage();
+	            protected void paintComponent(Graphics g)
+	            {   g.drawImage(image2, 0, 0, getWidth(), getHeight(), null);
+	                g.drawImage(image, (getWidth()-image.getWidth(null))/2, (getHeight()-image.getHeight(null))/2, image.getWidth(null), image.getHeight(null), null);
+	               
+	                super.paintComponent(g);
+	            }
+	        };
+	        addAnimation(panel);
+	        addAnimation(panel2);
+	        panel2.setOpaque(false);
 	        panel.setOpaque(false);
 	        AnimatedObject.play("sounds/water/bubble.wav");
-//	      panel.add( new AnimationBackground(10, 10, 2, 3, 1, 1, 10) );
-	        panel.add( new AnimatedObject(300, 100, 3, 2, -1, 1, 20) );
-	        panel.add( new AnimatedObject(200, 200, 2, 3, 1, -1, 20) );
-	        panel.add( new AnimatedObject(50, 50, 5, 5, -1, -1, 20) );
-	        panel.add( new AnimatedObject(-200, 000, 3, 5, 1, 1, 20,"images/fish/smallfish.png"));
-	        panel.add( new AnimatedObject(0, 200, 5, 0, 1, 1, 80) );
-	        
 	        JFrame frame = new JFrame();
-	        frame.setContentPane(panel);
-	        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+	        JPanel cards = new JPanel(new CardLayout());
+	        cards.add(panel,"begin");
+	        cards.add(panel2,"end");
+	        frame.getContentPane().add(cards);
 	        frame.setExtendedState(Frame.MAXIMIZED_BOTH); 
 	        frame.setUndecorated(true);
 	        frame.setLocationRelativeTo( null );
 	        frame.setVisible(true);
-	        
+	        CardLayout cl = (CardLayout)(cards.getLayout());
+	        cl.show(cards, "begin");
 	        Thread.sleep(5000);
-	      
-	        frame.dispose();
-            Instructions window=new Instructions();
+	        cl.show(cards, "end");
+	        Thread.sleep(5000);
+	        ExperimenterWindow params = new ExperimenterWindow();
+			params.setVisible(true);
+       
 		
+	}
+	// this used to add animation to the background
+	public static void addAnimation (JPanel panel){
+		panel.add( new AnimatedObject(300, 100, 3, 2, -1, 1, 20) );
+        panel.add( new AnimatedObject(200, 200, 2, 3, 1, -1, 20) );
+        panel.add( new AnimatedObject(50, 50, 5, 5, -1, -1, 20) );
+        panel.add( new AnimatedObject(-200, 000, 3, 5, 1, 1, 20,filename));
+        panel.add( new AnimatedObject(0, 200, 5, 0, 1, 1, 80) );
 	}
 }
