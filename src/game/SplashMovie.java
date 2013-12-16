812b75d643e42d30e7590296a544bbefec01b2d8
fatima <fatima@129.64.235.201>
2013-12-15 20:05:37 -0500

    making the vedio (using cartoon ways)=)

--- src/game/SplashMovie.java
+++ src/game/SplashMovie.java
@@ -4,8 +4,12 @@ import java.awt.CardLayout;
 import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -14,7 +18,8 @@ public class SplashMovie {
 	public static  CardLayout cl;
 	public static  JPanel cards = new JPanel(new CardLayout());
 	public static String imagename="";
-	public static void main(String[] args) throws InterruptedException {
+	public static  BufferedImage image ;
+	public static void main(String[] args) throws InterruptedException, IOException {
 		JFrame frame = new JFrame();
 		frame.getContentPane().add(cards);
 	    frame.setExtendedState(Frame.MAXIMIZED_BOTH); 
@@ -22,7 +27,7 @@ public class SplashMovie {
 	    frame.setLocationRelativeTo( null );
 	    frame.setVisible(true);
 		ArrayList<JPanel> x=new ArrayList<JPanel>();
-		 for (int i=1; i<=100;i++)
+		 for (int i=1; i<=200;i++)
 		 {  
 			 if( i<10)
 				 imagename="000"+i+".jpg";
@@ -31,11 +36,10 @@ public class SplashMovie {
 			 else 
 				 imagename="0"+i+".jpg"; 
 			 System.out.println(imagename);
+			 image=ImageIO.read(new File ("images/splashMovie/"+imagename));
 		 JPanel panel = new JPanel(null)
 	        {   
 	       
-	            Image image = new ImageIcon("images/splashMovie/"+imagename).getImage();
-	           
 	            protected void paintComponent(Graphics g)
 	            {
 	                g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
@@ -48,8 +52,10 @@ public class SplashMovie {
 		  cards.add(panel,imagename)  ;  
 		  cl = (CardLayout)(cards.getLayout());
 		  cl.show(cards, imagename);
-		  Thread.sleep(250);
-		  panel.removeAll();
+		  Thread.sleep(40);
+		  panel=null;
+		  
+	     
 }
 		
 	}
